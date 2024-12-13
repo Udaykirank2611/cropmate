2b86c2df46215ddb5830d2653f3f2a9fadd1e26c
Uday kiran <76544318+Udaykirank2611@users.noreply.github.com>
2024-12-13 11:43:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,3 @@
 import streamlit as st
-import numpy as np
-import cv2
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import img_to_array
-import os
 
 st.title("heyyy")
